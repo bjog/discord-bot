077e1e6b5f11a804b96c5a3770971645e8f51695
Bhavish Jogeeah <bhavishj@hotmail.com>
2018-05-22 20:51:00 +0100

    Add support for Stellaris, ARMA 3 and Stardew Valley

--- bot.py
+++ bot.py
@@ -20,6 +20,19 @@ async def on_message(message):
 		reply = "@everyone **LAUNCH R6:SIEGE ->** steam://run/359550"
 		await client.send_message(message.channel,reply)
 
+	if message.content.startswith("!stellaris"):
+		reply = "@everyone **LAUNCH Stellaris ->** steam://run/281990"
+		await client.send_message(message.channel,reply)
+
+	if message.content.startswith("!stardew"):
+		reply = "@everyone **LAUNCH Stardew Valley ->** steam://run/413150"
+		await client.send_message(message.channel,reply)
+
+
+	if message.content.startswith("!arma3"):
+		reply = "@everyone **LAUNCH ARMA 3 ->** steam://run/107410"
+		await client.send_message(message.channel,reply)
+
 	if message.content.startswith("!getinfo"):
 		print(message.channel.id)
 		print(message.channel.name)
@@ -33,7 +46,7 @@ async def on_ready():
 	print(client.user.id)
 
 def load_token():
-	f = open("etc\\token.secret","r")
+	f = open("etc/token.secret","r")
 	token = f.readline()
 	f.close()
 	return token.strip('\n')
